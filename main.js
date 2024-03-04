78e25d843769c0a959cec8cdb2a6fcf2975165dc
yasser9947 <yasser9946.1@gmail.com>
2024-03-04 03:01:59 +0300

    page

---
+++ main.js
@@ -0,0 +1,51 @@
+const movieName = document.querySelector("#movieName");
+const category = document.querySelector("#category");
+const year = document.querySelector("#year");
+const image = document.querySelector("#image");
+const btn = document.querySelector("#btn");
+const aleart = document.querySelector("#aleart");
+const form = document.querySelector("#form");
+const thanks = document.querySelector("#thanks");
+var myHeaders = new Headers();
+myHeaders.append("accept", "application/json");
+myHeaders.append("content-type", "application/json");
+myHeaders.append("user-agent", "GuzzleHttp/7");
+
+
+
+
+btn.addEventListener("click" , ()=>{
+    console.log(movieName.value , category.value , year.value , image.value )
+    movieName.value
+    if ( !movieName.value || !category.value || !year.value || !image.value){
+        console.log("error")
+        aleart.style.display = "block"
+            setTimeout(()=> aleart.style.display = "none"  , 3000)
+        return
+    }
+    var raw = JSON.stringify({
+        "data": {
+            "name": movieName.value ,
+            "category": category.value,
+            "year": year.value,
+            "image" :image.value,
+        }
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://v1.slashapi.com/java-team/google-sheets/PK1gimkQMB/tete", requestOptions)
+        .then(response => response.text())
+        .then(result =>{
+            form.style.display = "none"
+            thanks.style.display = "block"
+
+        })
+        .catch(error => console.log('error', error));
+})
+
